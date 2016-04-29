3e50f85fd5b83200ca524e4726480fd5b233ecaf
Josh <jchang82@illinois.edu>
2016-04-29 17:05:11 -0500

    slime.cpp

--- stage_1/slime.cpp
+++ stage_1/slime.cpp
@@ -1,6 +1,9 @@
-class slime: public sf::Sprite
-{
-
-
-}
+#include <SFML/Graphics.hpp>
+class slime: public sf::Sprite{
+    public:
+        int x_pos, y_pos;
+        int mass; //kg
+        int health;
+        int attack_dmg; 
+};
 
