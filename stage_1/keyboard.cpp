6abb5162c15f74a67f9fd954330dc0e9a3d03038
Josh <jchang82@illinois.edu>
2016-04-29 02:32:11 -0500

    cleaned up code

--- stage_1/keyboard.cpp
+++ stage_1/keyboard.cpp
@@ -10,8 +10,8 @@ void gravity(sf::Shape shape, int & x_move, int & y_move, int window_x, int wind
     */
 }
 
-void move_shape(sf::Shape & shape, int & x_vel, int & y_vel, int vel, int max_vel){
-    shape.move(x_vel, y_vel);
+void move_sprite(sf::Sprite & sprite, int & x_vel, int & y_vel, int vel, int max_vel){
+    sprite.move(x_vel, y_vel);
     x_vel += vel;
     y_vel += vel;
     if(x_vel <= 0 && x_vel <= max_vel ||
@@ -20,10 +20,10 @@ void move_shape(sf::Shape & shape, int & x_vel, int & y_vel, int vel, int max_ve
             y_vel > 0 && y_vel >= max_vel){y_vel = max_vel;}
 }
 
-void stop_shape(sf::Shape & shape, int & x_vel, int & y_vel, int accel){
+void stop_sprite_x(sf::Sprite & sprite, int & x_vel, int & y_vel, int accel){
     x_vel -= x_vel * accel;
     y_vel -= y_vel * accel;
-    shape.move(x_vel, y_vel);
+    sprite.move(x_vel, y_vel);
     x_vel = 0;
 }
 
--- stage_1/stage_1.cpp
+++ stage_1/stage_1.cpp
@@ -20,8 +20,8 @@ private:
 void draw_all(){
     window.clear();
     window.draw(player);
-    for (size_t i = 0; i < mobs.size(); i++)
-	window.draw(mobs[i]);
+    for (size_t i = 0; i < mobs.size(); i++){
+    	window.draw(mobs[i]);}
     window.display();
 }
 
@@ -30,24 +30,23 @@ void initialize_player(){
     sf::Texture player_texture;
     player_texture.loadFromFile("player.png");
     player.setTexture(player_texture);
-    player.setTextureRect(sf::IntRect(10,10,50,30));
-    player.setColor(sf::Color(255,255,255,200));
-    player.setPosition(100,25);
+    player.setTextureRect(sf::IntRect(10, 10, 50, 30));
+    player.setColor(sf::Color(255, 255, 255, 200));
+    player.setPosition(100, 25);
 }
 
 void initialize_mobs(){
     sf::Texture mob_texture;
     mob_texture.loadFromFile("mob.png");
 
-    for(int i = 0; i <5; i++)
-    {
-	sf::Sprite mob(mob_texture);
-	mob.setTextureRect(sf::IntRect(10,10,50,30));
-	mob.setColor(sf::Color(255, 0, 0, 200));
-	mob.setPosition(rand()%10,25);	
-	mobs.push_back(mob);
-    }
+    for(int i = 0; i < 5; i++){
+        sf::Sprite mob(mob_texture);
+        mob.setTextureRect(sf::IntRect(10,10,50,30));
+        mob.setColor(sf::Color(255, 0, 0, 200));
+        mob.setPosition(rand()%10,25);	
+        mobs.push_back(mob);}
 }
+
 //checks for system events
 void system_events(){
     if(event.type == sf::Event::Closed){window.close();}
@@ -59,32 +58,18 @@ void system_events(){
 //keyboard inputs (pressed)
 void key_pressed_events(){
     if(event.key.code == sf::Keyboard::Left){
-        move_shape(square, x_move, 0, -2, -5);}
+        move_sprite(player, x_move, 0, -2, -5);}
 
     else if(event.key.code == sf::Keyboard::Right){
-        move_shape(square, x_move, 0, 2, 5);}
+        move_sprite(player, x_move, 0, 2, 5);}
 }
 
 //keyboard inputs (release)
 void key_released_events(){
    if(event.key.code == sf::Keyboard::Left){
-        square.move((x_move) / 2, 0);
-        x_move = 0;}
-
+        stop_shape_x(player, x_vel, y_vel, .5);
    else if(event.key.code == sf::Keyboard::Right){
-        square.move(x_move / 2, 0);
-        x_move = 0;}
-}
-
-//set square (user) position
-void set_square_position(){
-    sf::Vector2f v = square.getPosition();
-
-    if(v.x < 0){
-        square.setPosition(0, window_y - shape_size);}
-
-    else if(v.x > window_x){
-        square.setPosition(window_x - shape_size, window_y - shape_size);}
+        stop_shape.x(player, x_vel, y_vel, .5);
 }
 
 //main function
@@ -105,7 +90,6 @@ int main(){
                 key_released_events();}
             }
 
-            set_square_position();
             draw_all();
     }
     return EXIT_SUCCESS;
