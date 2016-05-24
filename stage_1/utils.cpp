dd5067fe254544beca656549ef29da67e3f0555c
Josh <jchang82@illinois.edu>
2016-05-24 16:04:44 -0500

    plz fix movement

--- stage_1/utils.cpp
+++ stage_1/utils.cpp
@@ -2,9 +2,11 @@
 #include "extended_sprite.cpp"
 
 void flip_sprite_left(Extended_Sprite & sprite){
-    sprite.setScale(-1, 1);
+    sf::Vector2f scl = sprite.getScale();
+    sprite.setScale(-1 * scl.x, 1 * scl.y);
 }
 
 void flip_sprite_right(Extended_Sprite & sprite){
-   sprite.setScale(1, 1); 
+   sf::Vector2f scl = sprite.getScale();
+   sprite.setScale(1 * scl.x, 1 * scl.y); 
 }
